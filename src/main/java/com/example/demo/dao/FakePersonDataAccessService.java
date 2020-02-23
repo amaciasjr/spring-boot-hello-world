e3bab279f21b060b291214c1558fb9de54296b2f
Arturo Macias <Arturo.Macias@familysearch.org>
2020-02-22 22:55:38 -0700

    Add functionality to get single person from DB

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -19,9 +20,25 @@ public class FakePersonDataAccessService implements PersonDao {
   }
 
   @Override
+  public Optional<Person> selectPersonById(UUID id) {
+    return DB.stream()
+            .filter(person -> person.getId().equals(id))
+            .findFirst();
+  }
+
+  @Override
   public List<Person> selectAllPeople() {
     return DB;
   }
 
+  @Override
+  public int deletePersonById(UUID id) {
+    return 0;
+  }
+
+  @Override
+  public int updatePersonById(UUID id, Person person) {
+    return 0;
+  }
 
 }
--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -14,6 +15,11 @@ public interface PersonDao {
     return insertPerson(id, person);
   }
 
+  Optional<Person> selectPersonById(UUID id);
+
   List<Person> selectAllPeople();
 
+  int deletePersonById(UUID id);
+
+  int updatePersonById(UUID id, Person person);
 }
